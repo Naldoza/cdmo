01d8eb7844b51b0c89d4ab0f37bf9612339860b5
lewyeR <itc.conise@gmail.com>
2018-05-24 15:57:47 +0800

    first

---
+++ CDMO-JOO/app/Http/Controllers/foremanController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class foreman extends Controller
+{
+    public function ForemanForm1() {
+
+    	return view('Foreman.ForemanForm');
+
+    }
+}
